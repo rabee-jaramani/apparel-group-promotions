e754b775bd972648ed2531733e6b2e189d0c8b51
Rabee Aljaramani <rabeejermani@gmail.com>
2023-03-17 13:52:11 +0400

    d

--- src/banner/Banner.js
+++ src/banner/Banner.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import apparel_promotions_banner from '../assets/apparel_promotions_banner.jpg'
+import apparel_promotions_banner from '../assets/apparel_promotions_banner.svg'
 export default function Banner() {
     return (
         <div className='banner-cont'>
-            <img src={apparel_promotions_banner} alt='promotions banner' />
+            {/* <img src={apparel_promotions_banner} alt='promotions banner' /> */}
         </div>
     )
 }
