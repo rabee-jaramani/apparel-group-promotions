ff7b627c9b3be90ebfcc3e216f94b29aa4e07ea2
Rabee Aljaramani <rabeejermani@gmail.com>
2023-03-22 11:55:15 +0400

    data

--- src/card/Card.js
+++ src/card/Card.js
@@ -1,32 +1,48 @@
-import React from 'react'
+import React from 'react';
 
 export default function Card(item) {
-    const el = item.item;
-    return (
-        <div className='card-cont'>
-            {/* 2022 has no images so do not show images */}
-            {el.year === 2023 ? <img src={el.image} alt='default-ghgh' /> : ''}
-            <div className='info-div'>
-                <p><strong>Name</strong></p>
-                <p>{el.name}</p>
-            </div>
-            <div className='info-div'>
-                <p><strong>New Designation</strong></p>
-                <p>{el.new_designation}</p>
-            </div>
-            <div className='info-div'>
-                <p><strong>Brand</strong></p>
-                <p>{el.brand}</p>
-            </div>
-            <div className='info-div'>
-                <p><strong>Region</strong></p>
-                <p>{el.country}</p>
-            </div>
-            <div className='info-div'>
-                <p><strong>Date</strong></p>
-                <p>{el.year} / {el.month}</p>
-            </div>
+  const el = item.item;
+  return (
+    <div className="card-cont">
+      {/* 2022 has no images so do not show images */}
+      {el.year === 2023 ? <img src={el.image} alt="default-ghgh" /> : ''}
+      <div className="info-div">
+        <p>
+          <strong>Name</strong>
+        </p>
+        <p className="p-2">{el.name}</p>
+        {/* <div className="line"></div> */}
+      </div>
+      <div className="info-div">
+        <p>
+          <strong>New Designation</strong>
+        </p>
+        <p className="p-2">{el.new_role}</p>
+      </div>
 
-        </div>
-    )
+      <div className="info-div">
+        <p>
+          <strong>Brand</strong>
+        </p>
+        <p className="p-2">{el.brand}</p>
+      </div>
+
+      <div className="info-div">
+        <p>
+          <strong>Region</strong>
+        </p>
+        <p className="p-2">{el.country}</p>
+      </div>
+
+      <div className="info-div">
+        <p>
+          <strong>Date</strong>
+        </p>
+        <p className="p-2">
+          {el.year}
+          {el.month ? `/${el.month}` : ''}
+        </p>
+      </div>
+    </div>
+  );
 }
