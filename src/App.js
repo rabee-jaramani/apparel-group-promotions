64d392d0314d0f079fcc922baf0f1635946f076f
Rabee Aljaramani <rabeejermani@gmail.com>
2023-04-07 14:34:57 +0400

    footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Banner from './banner/Banner';
 import Body from './body/Body';
+import Footer from './footer/Footer';
 import Header from './header/Header';
-import './style.sass'
+import './style.sass';
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
       <Body />
+      <Footer />
     </div>
   );
 }
---
+++ src/footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className="footer-cont">
+      <div class="logo-div">
+        <img
+          src="/apparel-promotions/static/media/ag_logo.d91f35808d094cfb863c8a6d796e6bbc.svg"
+          alt="ag logo"
+        />
+      </div>
+      <a
+        rel="noreferrer"
+        target="_blank"
+        href="https://apparelgroupglobal.com/en/"
+      >
+        www.apparelgroupglobal.com
+      </a>
+      <p className="powered-by">Powered By Apparel Group</p>
+    </div>
+  );
+}
